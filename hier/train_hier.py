2ab802f53ef459b26a36da2e644e9eb2d7108209
Rose-PhD <rosemary.nalwanga@utwente.nl>
2026-04-08 21:40:12 +0300

    updated the dataset name

--- hier/train_hier.py
+++ hier/train_hier.py
@@ -212,8 +212,8 @@ def train_fold(fold, model, train_loader, val_loader, device, fold_dir, crops,
 
 # main
 def main():
-    DATASET = "/deepstore/datasets/dmb/ComputerVision/biology/training7"
-    SAVE_ROOT = "/home/nalwangar/finally/logs_hierM"
+    DATASET = "/deepstore/datasets/dmb/ComputerVision/biology/train-V"
+    SAVE_ROOT = "/home/nalwangar/finally/logs_hierX"
     os.makedirs(SAVE_ROOT, exist_ok=True)
 
     crops, diseases_by_crop, items = build_index(DATASET)
